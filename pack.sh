504b2e54ad48100624483b3784365e17dba8a584
Jirachai C <jirachai.c@outlook.com>
2025-04-10 19:22:16 +0800

    Update MongoDB CSFLE migration scripts: add create-schema.js for user collection, enhance pack.sh with build checks, and bump version to 1.0.2a-SNAPSHOT.

--- pack.sh
+++ pack.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+cd ./mongodb-csfle-migrator
+# Build the project
+mvn clean package -DskipTests
+# Check if the build was successful
+if [ $? -ne 0 ]; then
+  echo "Build failed. Exiting."
+  exit 1
+fi
+# Check if the target directory exists
+if [ ! -d "target/MongoDBCSFLEMigrator" ]; then
+  echo "Target directory does not exist. Exiting."
+  exit 1
+fi
+
+cd ..
+
 RELEASES_FOLDER="./releases"
 RELEASES_FOLDER=$(realpath "$RELEASES_FOLDER")
 RELEASES_FILES_FOLDER="$RELEASES_FOLDER/files"
@@ -10,6 +27,11 @@ cp ./mongodb-csfle-migrator/target/MongoDBCSFLEMigrator/*.jar $RELEASES_FILES_FO
 # Copy the sample JSON files to the releases directory
 # and rename them to remove the .sample extension
 for file in ./mongodb-csfle-migrator/*.sample.json; do
+  cp "$file" "$RELEASES_FILES_FOLDER/$(basename "${file/.sample/}")"
+done
+
+# and rename them to remove the .sample extension
+for file in ./*.sample.mongodb.js; do
   cp "$file" "$RELEASES_FILES_FOLDER/$(basename "${file/.sample/}")"
 done
 
